855e6c2477c9a05b447c58c5473819da132653af
Victor Maslov <nakilon@gmail.com>
2016-11-06 02:02:46 +0300

    moving oneplus_mod bot to GCP

--- examples/oneplus/main.rb
+++ examples/oneplus/main.rb
@@ -1,21 +1,23 @@
 ﻿require_relative "../boilerplate"
 
 BOT = RedditBot::Bot.new YAML.load(File.read "secrets.yaml"), ignore_captcha: true
-
-require_relative "#{"../" if ENV["LOGNAME"] == "nakilon"}../../../dimensioner/get_dimensions"
-
 SUBREDDIT = "oneplus"
-# FLAIR_CLASS = "redflair"
+
+if Gem::Platform.local.os == "darwin"
+  require_relative "../../../../dimensioner/get_dimensions"
+else
+  require_relative "#{Dir.home}/get_dimensions"
+end
 
 checked = []
 loop do
-  AWSStatus::touch
+  Hearthbeat.beat "u_oneplus_mod_r_oneplus", 310 unless Gem::Platform.local.os == "darwin"
   puts "LOOP #{Time.now}"
 
-  BOT.json(:get, "/r/#{SUBREDDIT}/new")["data"]["children"].each do |item|
-    id, url, title, subreddit = item["data"].values_at(*%w{ id url title subreddit })
-    next if checked.include? id # WTF?
-    checked << id
+  BOT.json(:get, "/r/#{SUBREDDIT}/new")["data"]["children"].each do |post|
+    id, url, title, subreddit = post["data"].values_at(*%w{ id url title subreddit })
+    next if checked.include? id
+    checked.push id
     redd_it = "https://redd.it/#{id}"
     next puts "skipped #{url} from #{redd_it}" if :skipped == _ = GetDimensions::get_dimensions(url)
     next puts "unable #{url} from #{redd_it}" unless _
@@ -23,18 +25,6 @@ loop do
     result = ([1080, 1920] != resolution = [width, height])
     puts "#{result} #{id} [#{resolution}] #{title} #{url}"
     next if result
-
-    ### flair
-    # {"json"=>{"errors"=>[]}} == _ = BOT.json(:post,
-    #   "/api/selectflair",
-    #     flair_template_id: BOT.json(:post,
-    #       "/api/flairselector",
-    #         link: "t3_#{id}",
-    #     )["choices"].find{ |i| i["flair_css_class"] == FLAIR_CLASS }["flair_template_id"],
-    #     link: "t3_#{id}",
-    # ) or fail _.inspect
-    ### report
-    # BOT.report "1080x1920", "t3_#{id}"
 
     ### delete
     BOT.json :post, "/api/remove",
