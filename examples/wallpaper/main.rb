4cdf387cf464ee53ef8f63552bfd2e3ad0e606ef
Victor Maslov <nakilon@gmail.com>
2017-06-04 05:08:59 +0300

    needs_captcha

--- examples/wallpaper/main.rb
+++ examples/wallpaper/main.rb
@@ -1,6 +1,6 @@
 ﻿require_relative "../boilerplate"
 
-BOT = RedditBot::Bot.new YAML.load(File.read "secrets.yaml"), ignore_captcha: true
+BOT = RedditBot::Bot.new YAML.load File.read "secrets.yaml"
 SUBREDDIT = "wallpaper"
 
 if Gem::Platform.local.os == "darwin"
