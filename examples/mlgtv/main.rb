9ea0a869b72d0063e82a9b2ce38e3e0b1e288bf4
Victor Maslov <nakilon@gmail.com>
2017-08-18 23:45:58 +0300

    added markers of what the game is

--- examples/mlgtv/main.rb
+++ examples/mlgtv/main.rb
@@ -1,6 +1,6 @@
 require_relative File.join "../boilerplate"
 
-BOT = RedditBot::Bot.new YAML.load(File.read "secrets.yaml"), ignore_captcha: true
+BOT = RedditBot::Bot.new YAML.load File.read "secrets.yaml"
 
 SUBREDDIT = "codcompetitive"
 
@@ -13,8 +13,11 @@ loop do
       throw :loop unless statuses = JSON.parse(
         NetHTTPUtils.request_data("http://streamapi.majorleaguegaming.com/service/streams/all")[/\{.+\}/m]
       )["data"]["items"]
+      games = JSON.parse(
+        NetHTTPUtils.request_data("http://www.majorleaguegaming.com/api/games/all")[/\{.+\}/m]
+      )["data"]["items"]
       begin
-        JSON.parse NetHTTPUtils.request_data "http://www.majorleaguegaming.com/api/channels/all.js?fields=name,url,tags,stream_name"
+        JSON.parse NetHTTPUtils.request_data "http://www.majorleaguegaming.com/api/channels/all?fields=name,url,tags,stream_name,game_id"
       rescue JSON::ParserError
         puts "JSON::ParserError"
         sleep 60
@@ -23,9 +26,21 @@ loop do
         next unless item1["tags"].include? "COD Pro League"
         status = statuses.find{ |item2| item1["stream_name"] == item2["stream_name"] }
         next unless status && status["status"] > 0
+        game = games.find{ |game| game["id"] == item1["game_id"] }
         list << "* [](#mlg) [](##{
           ["?", "live", "replay"][status["status"]]
-        }) [**#{
+        }) #{
+          "[](##{ {
+            "Call of Duty: Modern Warfare 2" => "codmw2",
+            "Call of Duty: Modern Warfare 3" => "codmw3",
+            "Call of Duty: Black Ops" => "codbo",
+            "Call of Duty: Black Ops II" => "codbo2",
+            "Call of Duty: Black Ops III" => "codbo3",
+            "Call of Duty: Advanced Warfare" => "codaw",
+            "Call of Duty: Ghosts" => "codghosts",
+            "Call of Duty: Infinite Warfare" => "codiw",
+          }[game["name"]] }) " if game
+        }[**#{
           item1["name"]
         }**](#{
           item1["url"]
@@ -44,7 +59,7 @@ loop do
       ].each do |game|
         (begin
           begin
-            t = NetHTTPUtils.get_response "https://api.twitch.tv/kraken/streams?game=#{CGI::escape game}&access_token=#{File.read("twitch.token").strip}&client_id=#{File.read("client.id").strip}&channel=Ricky,ACHES,Lacefield,Clayster,Enable,Zoomaa,Attach,TheFEARS,MiRx1,SaintsRF,StuDyy,SpaceLyTV,NAMELESS,Scump,FORMAL,Crimsix,Karma,Loony,Slacked,Octane,MJChino,Diabolic_TV,ImTheIvy,Senderxz,Jkap,John,SlasheRAL,Apathy,ColtHavok,MikeSwarley,ParasiteTV,TyreeLegal,Silly,Blfire,methodz,TwiZzyTV,Mochila,Remy,Xotic16,AquA,Faccento,Nagafen,Tylerfelo,TheoryCoD,ColeChanTV,happyy97,goonjar,Burns,Dedo,Neslo,TeeCM,K1lla93,NeLsoNNaTeR,ProoFy,Whea7s,MBoZe,Merk,Nadeshot,ReeP,Sharp,TeePee,Braaain2015,Nolsonn,QwiKeRTHaNu,Zedenyer1,Jurd,Tommey,Swanny,MadCatEU,Rated_EU1,BsportJoshh,Sy_Vortex,TheMarkyB,Peatie95,urbandm,TreiZer0,iDqvee,Tojor,MethodZ_TV,Gotaga,WailersWL,TCM_Moose,skrapzq,Reedy,fighta71,Swiftazor,BacabecNZ,Zeuss_Gaming,Hopeyy,GuydraCOD,mattmrx,Maven,CouRageJD,Revan,BriceyHD,Benson,PHILWHI7,MLG,mlgbravo,Multiplay_CoD,UMGEvents,GfinityTV,WorldGaming,FemaleProLeague,Arcitys,Prestinni,Maux,priestahh,Vilesyder,benbance"
+            t = NetHTTPUtils.get_response "https://api.twitch.tv/kraken/streams?game=#{CGI::escape game}&access_token=#{File.read("twitch.token").strip}&client_id=#{File.read("client.id").strip}&channel=#{File.read("channels.txt").split.join ?,}"
           end while t.code == 500
           JSON.parse t.body
         rescue JSON::ParserError
@@ -52,7 +67,12 @@ loop do
           sleep 60
           retry
         end["streams"] || []).each do |channel|
-          list << "* [](#twitch) [](#live) [**#{
+          list << "* [](#twitch) [](#live) #{
+            "[](##{ {
+              "Call of Duty: Infinite Warfare" => "codiw",
+              "Call of Duty 4: Modern Warfare" => "cod4",
+            }[channel["game"]] }) "
+          }[**#{
             channel["channel"]["display_name"]
           }**](#{
             channel["channel"]["url"]
