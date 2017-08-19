4dbcb0dd46a1fa7f1255cc0f5cf1636959e399fa
Victor Maslov <nakilon@gmail.com>
2017-08-19 08:05:36 +0300

    Google Cloud Logging

--- examples/cptflairbot3/main.rb
+++ examples/cptflairbot3/main.rb
@@ -1,22 +1,25 @@
 ﻿require_relative "../boilerplate"
 BOT = RedditBot::Bot.new YAML.load File.read "secrets.yaml"
 
+require "gcplogger"
+logger = GCPLogger.logger "casualpokemontrades"
+
 loop do
-  Hearthbeat.beat "u_CPTFlairBot3_r_casualpokemontrades", 70 unless Gem::Platform.local.os == "darwin"
+  Hearthbeat.beat "u_CPTFlairBot3_r_casualpokemontrades", 70 if Google::Cloud.env.compute_engine?
 
   unread = BOT.json :get, "/message/unread"
   unread["data"]["children"].each do |msg|
-    next puts "bad destination: #{msg["data"]["dest"]}" unless msg["data"]["dest"] == "CPTFlairBot3"
+    next logger.info "bad destination: #{msg["data"]["dest"]}" unless msg["data"]["dest"] == "CPTFlairBot3"
     case msg["data"]["subject"]
     when "casualpokemontrades"
       unless /^(?<name>\S+( \S+)*) ?\n(?<id>\d\d\d\d-\d\d\d\d-\d\d\d\d)\n(?<css_class>\S+)$/ =~ msg["data"]["body"]
-        puts "invalid message for #{msg["data"]["subject"]}: %p" % msg["data"]["body"] if Gem::Platform.local.os == "darwin"
+        logger.info "invalid message for #{msg["data"]["subject"]}: %p" % msg["data"]["body"] unless Google::Cloud.env.compute_engine?
         # puts "marking invalid message as read: %p" % msg["data"]["body"]
         # BOT.json :post, "/api/read_message", {id: msg["data"]["name"]} unless Gem::Platform.local.os == "darwin"
         next
       end
       if name.size > 50
-        puts "too large name: %p" % name if Gem::Platform.local.os == "darwin"
+        logger.info "too large name: %p" % name unless Google::Cloud.env.compute_engine?
         next
       end
       begin
@@ -27,21 +30,21 @@ loop do
             "relaxedpokemontrades" => "#{name} #{id}",
           }[msg["data"]["subject"]],
           css_class: css_class,
-        }.tap{ |h| puts "setting up flair at /r/#{msg["data"]["subject"]}: #{h}" }
+        }.tap{ |h| logger.warn "setting up flair at /r/#{msg["data"]["subject"]}: #{h}" }
       rescue RuntimeError => e
         if e.to_s == "BAD_FLAIR_TARGET"
-          puts "#{e.to_s}: '#{msg["data"]["author"]}'"
+          logger.error "#{e.to_s}: '#{msg["data"]["author"]}'"
         else
           raise e
         end
       end
       BOT.json :post, "/api/read_message", {id: msg["data"]["name"]}
     else
-      next puts "bad subject: #{msg["data"]["subject"]}"
+      next logger.debug "bad subject: #{msg["data"]["subject"]}"
     end
     break # just for a case
   end
 
-  puts "END LOOP #{Time.now}" if Gem::Platform.local.os == "darwin"
+  logger.info "END LOOP #{Time.now}" unless Google::Cloud.env.compute_engine?
   sleep 60
 end
