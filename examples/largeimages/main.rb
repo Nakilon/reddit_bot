8d199cfebfac175457245f9a22ea3d5215528c99
Victor Maslov <nakilon@gmail.com>
2017-11-13 19:51:14 +0300

    gcplogger

--- examples/largeimages/main.rb
+++ examples/largeimages/main.rb
@@ -3,7 +3,11 @@
 
 require "nokogiri"
 
+require "gcplogger"
+logger = GCPLogger.logger "feedpcbr"
+
 require_relative "../get_dimensions"
+Imgur.logger = logger
 
 
 require "../boilerplate"
@@ -26,7 +30,7 @@ EXCLUDE = %w{ foodporn powerwashingporn }
 checked = []
 
 loop do
-  puts "LOOP #{Time.now}"
+  logger.warn "LOOP #{Time.now}"
 
   [ [:source_ultireddit, 5000000, ( Nokogiri::XML(NetHTTPUtils.request_data ENV["FEEDPCBR_URL"]).remove_namespaces!.xpath("feed/entry").map do |entry|
     [
@@ -48,31 +52,34 @@ loop do
     end.compact
   end ) ],
   ].each do |source, min_resolution, entries|
-    entries..each do |id, url, title, subreddit, author, permalink|
+    logger.warn "#{source}.size: #{entries.size}"
+    entries.each do |id, url, title, subreddit, author, permalink|
       next if checked.include? id
       checked << id
       # next if Gem::Platform.local.os == "darwin" # prevent concurrent posting
+      logger.debug "image url: #{url}"
 
-      next puts "skipped (GetDimensions :skipped) #{url} from http://redd.it/#{id}" if :skipped == _ = begin
+      next logger.warn "skipped (GetDimensions :skipped) #{url} from http://redd.it/#{id}" if :skipped == _ = begin
         GetDimensions::get_dimensions url
       rescue GetDimensions::Error404
-        next puts "skipped (GetDimensions::Error404) #{url} from http://redd.it/#{id}"
+        next logger.warn "skipped (GetDimensions::Error404) #{url} from http://redd.it/#{id}"
       rescue GetDimensions::ErrorUnknown
-        next puts "skipped (GetDimensions::ErrorUnknown) #{url} from http://redd.it/#{id}"
+        next logger.warn "skipped (GetDimensions::ErrorUnknown) #{url} from http://redd.it/#{id}"
       end
       fail "unable #{url} from http://redd.it/#{id}" unless _
       width, height, best_direct_url, *all_direct_urls = _
+      logger.info "GetDimensions: %p" % [[width, height, best_direct_url, all_direct_urls.size]]
       unless min_resolution <= width * height
-        next puts "skipped low resolution #{source}"
+        next logger.warn "skipped low resolution #{source}"
       end
       # next if Gem::Platform.local.os == "darwin" # prevent concurrent posting
       # puts "https://www.reddit.com/r/LargeImages/search.json?q=url%3A#{CGI.escape url}&restrict_sr=on"
       resolution = "[#{width}x#{height}]"
       # require "cgi"
-      next puts "already submitted #{resolution} #{id}: '#{url}'" unless
+      next logger.warn "already submitted #{resolution} #{id}: '#{url}'" unless
         Gem::Platform.local.os == "darwin" ||
         (JSON.parse NetHTTPUtils.request_data "https://www.reddit.com/r/LargeImages/search.json?q=url%3A#{CGI.escape url}&restrict_sr=on", header: ["User-Agent", "ajsdjasdasd"])["data"]["children"].empty?
-      puts "#{resolution} got from #{id}: #{url}"
+      logger.warn "resolution #{resolution} got from #{id}: #{url}"
       # next if Gem::Platform.local.os == "darwin" # prevent concurrent posting
       title = "#{resolution}#{
         " [#{all_direct_urls.size} images]" if all_direct_urls.size > 1
@@ -82,7 +89,7 @@ loop do
               sub(/(.{#{100 - subreddit.size}}).+/, '\1...')
       } /r/#{subreddit}".
         gsub(/\s+\(\s+\)\s+/, " ")
-      puts "new post #{source}: #{url} #{title.inspect}"
+      logger.warn "new post #{source}: #{url} #{title.inspect}"
       unless Gem::Platform.local.os == "darwin"
         result = BOT.json :post,
           "/api/submit",
@@ -93,7 +100,7 @@ loop do
             title: title,
           }
         next unless result["json"]["errors"].empty?
-        puts result["json"]["data"]["url"]
+        logger.info "post url: #{result["json"]["data"]["url"]}"
       end
         # {"json"=>
         #   {"errors"=>[],
@@ -109,11 +116,11 @@ loop do
         all_direct_urls - [best_direct_url]
       ] if all_direct_urls.size > 1
       text = [line1, line2, line3].compact.join("  \n")
-      puts "new comment: #{text.inspect}"
+      logger.info "new comment: #{text.inspect}"
       unless Gem::Platform.local.os == "darwin"
         result = BOT.leave_a_comment "#{result["json"]["data"]["name"]}", text.sub(/(?<=.{9000}).+/m, "...")
         unless result["json"]["errors"].empty?
-          p result
+          logger.error result.inspect
           fail "failed to leave comment"
         end
       end
@@ -122,6 +129,6 @@ loop do
     end
   end
 
-  puts "END LOOP #{Time.now}"
+  logger.warn "END LOOP #{Time.now}"
   sleep 300
 end
