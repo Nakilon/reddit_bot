5b6bff4ff821cb03315755fcc7edbe0275487d17
Victor Maslov <nakilon@gmail.com>
2018-12-05 02:14:38 +0300

    upgrade an old bot to use the directlink gem

--- examples/wallpaper/main.rb
+++ examples/wallpaper/main.rb
@@ -3,29 +3,23 @@
 BOT = RedditBot::Bot.new YAML.load File.read "secrets.yaml"
 SUBREDDIT = "wallpaper"
 
-if Gem::Platform.local.os == "darwin"
-  require_relative "../../../../dimensioner/get_dimensions"
-else
-  require_relative "#{Dir.home}/get_dimensions"
-end
+require "directlink"
 
 checked = []
 loop do
-  Hearthbeat.beat "u_wallpaperpedantbot_r_#{SUBREDDIT}", 70 unless Gem::Platform.local.os == "darwin"
   puts "LOOP #{Time.now}"
 
   BOT.json(:get, "/r/#{SUBREDDIT}/new")["data"]["children"].each do |post|
     id, url, title, subreddit = post["data"].values_at(*%w{ id url title subreddit })
     next if checked.include? id
     checked.push id
-    next puts "skipped #{url} from http://redd.it/#{id}" if :skipped == _ = GetDimensions::get_dimensions(url)
-    next puts "unable #{url} from http://redd.it/#{id}" unless _
-    width, height, best_direct_url, *all_direct_urls = _
-
-    resolution = "#{width}x#{height}"
-    result = title[/\s*\[?#{width}\s*[*x×]\s*#{height}\]?\s*/i]
-    puts "#{!!result} #{id} [#{resolution}] #{title} #{url}"
-    BOT.report "true resolution is #{resolution}", "t3_#{id}" unless result
+    t = DirectLink url
+    (t.is_a?(Array) ? t : [t]).each do |s|
+      resolution = "#{s.width}x#{s.height}"
+      result = title[/\s*\[?#{s.width}\s*[*x×]\s*#{s.height}\]?\s*/i]
+      puts "#{!!result} #{id} [#{resolution}] #{title} #{s.url}"
+      BOT.report "true resolution is #{resolution}", "t3_#{id}" unless result
+    end
   end
 
   puts "END LOOP #{Time.now}"
