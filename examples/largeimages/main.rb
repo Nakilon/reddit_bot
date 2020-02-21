21b3e6d4cc207720c5878b48c1e99bf544666649
Victor Maslov <nakilon@gmail.com>
2020-02-21 06:27:37 +0300

    oops, commit was done in browser

--- examples/largeimages/main.rb
+++ examples/largeimages/main.rb
@@ -94,8 +94,6 @@ loop do
       next logger.warn "skipped a post by /u/redisforever"    if author == "redisforever"    # opt-out
       next logger.warn "skipped a post by /u/bekalaki"        if author == "bekalaki"        # 9 ways to divide a karmawhore
       next logger.warn "skipped a post by /u/cherryblackeyes" if author == "cherryblackeyes" # not nice
-      
-      https://www.reddit.com/r/LargeImages/comments/erg3b5/4977x6060_copied_features_of_thr_indian/ff3g4iy/
 
       t = begin
         DirectLink url, 60
